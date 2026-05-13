e9bfd13183b0dd290d1a3d91ff1075b11b94a731
SofiaSherman <sofiia.sherman@live.u-tad.com>
2026-05-13 11:35:01 +0200

    completed enemy exploded script, didnt test

--- Assets/Scripts/EnemyBehaviors/EnemyExplode.cs
+++ Assets/Scripts/EnemyBehaviors/EnemyExplode.cs
@@ -1,9 +1,25 @@
+using Unity.VisualScripting;
 using UnityEngine;
 
 public class EnemyExplode : MonoBehaviour
 {
+    [SerializeField] private float explosionRadius;
+    [SerializeField] private float damage;
+    [SerializeField] private float explosionStrength;
     public void Explode()
     {
-        Debug.Log("Explode");
+        Collider[] exploded = Physics.OverlapSphere(transform.position, explosionRadius);
+        foreach(Collider c in exploded)
+        {
+            if (!c.gameObject.TryGetComponent(out IDamageable damageable))
+            {
+                if (c.gameObject.tag == "Player")
+                {
+                    damageable.TakeDamage(damage);
+                }
+                c.GetComponent<Rigidbody>().AddForce(-transform.position + transform.up * explosionStrength, ForceMode.Impulse);
+            }
+        }
+        Destroy(gameObject);
     }
 }
